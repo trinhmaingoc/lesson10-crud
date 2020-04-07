43187af30d7024514310ffc1102b5ff6578d233f
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-07 10:00:45 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -105,8 +105,59 @@ export class App extends Component {
                   </ul>
                 </div>
               </div>
-
-
+            </div>
+            {/* List */}
+            <div className="row mt-15">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th className="text-center">STT</th>
+                      <th className="text-center">Tên</th>
+                      <th className="text-center">Trạng Thái</th>
+                      <th className="text-center">Hành Động</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td></td>
+                      <td>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="filterName"
+                        />
+                      </td>
+                      <td>
+                        <select
+                          name="filterStatus"
+                          className="form-control"
+                        >
+                          <option value={-1}>Tất Cả</option>
+                          <option value={0}>Ẩn</option>
+                          <option value={1}>Kích Hoạt</option>
+                        </select>
+                      </td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>Học React JS</td>
+                      <td className="text-center">
+                        <span className="label label-danger">Kích Hoạt</span>
+                      </td>
+                      <td className="text-center">
+                        <button type="button" className="btn btn-sm btn-warning">
+                          <span className="fa fa-pencil mr-5"></span>Sửa
+                        </button> &nbsp;
+                        <button type="button" className="btn btn-sm btn-danger">
+                          <span className="fa fa-trash mr-5"></span>Xóa
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
 
